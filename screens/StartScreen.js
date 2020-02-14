d68ad5833e4696d095db00b0503b4eb4cc5a9920
lolotrgeek <lolotrgeek@gmail.com>
2020-02-14 09:08:02 -0600

    state does not update from cold start, force it

--- screens/StartScreen.js
+++ screens/StartScreen.js
@@ -1,29 +1,33 @@
-import React, { useState, useEffect } from 'react'
-import { Text, View, SafeAreaView, SectionList, Button } from 'react-native'
+import React, { useState, useEffect, useRef } from 'react'
+import { Text, View, SafeAreaView, Button } from 'react-native'
 import useAsync from 'react-use/lib/useAsync';
 // import { getAll, storeItem, updateItem, removeAll } from '../constants/Store'
 import { getAll, storeItem, updateItem, removeAll, multiGet } from '../constants/Gun'
 import { styles } from '../constants/Styles'
-import { set } from 'date-fns';
 
 export default function StartScreen({ navigation }) {
     useEffect(() => navigation.setOptions({ title: 'Start' }), [])
     const [items, setItems] = useState([])
 
-    const setEntryState = async () => {
-        try {
-            const Items = await multiGet()
-            setItems(Items)
-        } catch (error) {
-            console.warn(error)
-        }
+    const setEntryState = async () => await multiGet()
 
-    }
+
 
     useEffect(() => {
-        setEntryState()
+        const timer = setInterval(async () => {
+            if (items.length > 0) clearInterval(timer)
+            
+            let Items = await setEntryState()
+            setItems(Items)
+            console.log(items)
+            console.log(items.length)
+        }, 1000); 
+        return () => clearInterval(timer);
+    }, []);
 
-    }, [])
+    // useAsync(async () => {
+    //     setItems(['hello'])
+    // }, [])
 
     return (
         <SafeAreaView style={styles.container}>
@@ -34,14 +38,21 @@ export default function StartScreen({ navigation }) {
                 lastscreen: 'Start'
             })} />
             <Button title="Load Items" onPress={async () => await setEntryState()} />
+            <Button title="Clear Items" onPress={async () => await setItems([])} />
             <View style={{ width: '100%' }}>
                 {items.map(item => {
-                    return (
-                        <View key={item[0]}>
-                            <Text >{item[0]}</Text>
-                            <Text>{item[1] ? item[1].type : 'null'}</Text>
-                        </View>
-                    )
+                    if (Array.isArray(item)) {
+                        return (
+                            <View key={item[0]}>
+                                <Text >{item[0]}</Text>
+                                <Text>{item[1] ? item[1].type : 'null'}</Text>
+                            </View>
+                        )
+                    }
+                    else {
+                        return (<Text key={item + Date.now()}>{item}</Text>)
+                    }
+
                 })}
             </View>
         </SafeAreaView >
